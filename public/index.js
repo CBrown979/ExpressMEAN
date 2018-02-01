9e5c356910473f34d6c2485f46c5563a20c19c13
Candice Bianca Brown <candicebrown97@gmail.com>
2018-02-01 23:05:56 +0000

    added jquery to the index.html file -- was able to remove the undefined $ error -- current error 404 (Not Found)

--- public/index.js
+++ public/index.js
@@ -1,17 +1,19 @@
 /* global $ */
 
 $(document).ready(function() {
-console.log('start');
+    console.log('start');
     $(function(){
-       $.get('/cities', appendToCities);
+      $.get('/cities', appendToCities);
        
-       function appendToCities(cities) {
-           var myCities = [];
-           for(var i in cities) {
-               console.log("hi");
-               myCities.push($('<option>', { text: cities[i] }));
-           }
-           $('#cityList').append(myCities);
+      function appendToCities(cities) {
+          var myCities = [];
+          for(var i in cities) {
+              console.log("hi");
+              myCities.push($('<option>', { text: cities[i] }));
+          }
+          $('#cityList').append(myCities);
         }
       });
+    console.log("end");
+
 });
