703ceeec460e71438f1cbdad18bf2d2b48ae2115
Candice Bianca Brown <candicebrown97@gmail.com>
2018-02-06 23:57:35 +0000

    initial query limit working - MEANEXpress/app.js - next, add Invalid Request, and all cities to appear with 0 query limit

--- app.js
+++ app.js
@@ -1,52 +1,27 @@
-//CodeSchool Express Level 1
-
-// Create a simple express node app meeting the following requirements
-// Requirements
-// Create an express app.
-// Create a root route that returns “Hello World”
-// Create a ‘/name’ route that returns your name
-// Create a /redirect route that sends you to /surprise with a moved permanently status code
-// Create a route that returns the current date. You will need to look up how to get the current date.
-
 var express = require ('express');
 var app = express();
-
-// app.get('/', function (request, response) {
-//   response.send("Hello World"); 
-// });
-// // app.listen(process.env.PORT);
-
-// app.get('/name', function (request, response){
-//     var name = "Candice B. Brown";
-//     response.send(name);
-// });
-
-// app.get('/surprise', function(request, response){
-//     response.send("SURPRISE!!!");
-// });
-
-// app.get('/name', function(request, response){
-//     response.redirect(301, '/surprise');
-// });
-
-// app.get('/date', function(request, response){
-//     var date = new Date();
-//     response.send(date);
-// });
-
-// //for Express Level 2
+//for Express Level 2 and used for Level 3 Assignment
 
 app.get('/cities', function(request, response){
     var cities = ['New York City', 'Toronto', 'San Diego', 'Providence', 'Boston'];
-    // response.json(cities);
-    if(request.query.limit > 0){
+    if (request.query.limit >= 0){
       response.json(cities.slice(0, request.query.limit));
-    } else if(request.query.limit > cities.length){
-      response.status(404).json("Invalid Request");
+      console.log('Hi');
     }
     else {
       response.json(cities);
     }
+    // if(request.query.limit >0){
+    //   response.json(cities.slice(0, request.query.limit));
+    // }
+    // if(request.query.limit > 0){
+    //   response.json(cities.slice(0, request.query.limit));
+    // } else {
+    //   response.status(404).json("Invalid Request");
+    // }
+    // else {
+    //   response.json(cities);
+    // }
 });
 
 // app.get('/index', function(request, response){
